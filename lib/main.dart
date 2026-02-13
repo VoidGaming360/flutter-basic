7193e74a482d94ee7a0f8d76baf0932676014dc2
Yalung Kiranti <46121363+VoidGaming360@users.noreply.github.com>
2026-02-13 17:58:48 +0545

    Add Counter, Events, Lists, Input and Controls

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,117 @@ class MyApp extends StatelessWidget {
     }
 }
 
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget{
     const HomePage({super.key});
 
     @override
+    State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    int counter = 0;
+    
+    void increment() {
+        setState(() {
+            counter++;
+        });
+    }
+
+    void decrement() {
+        setState (() {
+            counter--;
+        });
+    }
+
+    final TextEditingController nameController = TextEditingController();
+
+    final List<String> names = [];
+
+    void addName(){
+        final String text = nameController.text.trim();
+
+        if (text.isEmpty){
+            return;
+        }
+
+        setState(() {
+            names.add(text);
+            nameController.clear();
+        });
+    }
+
+    @override
+    void dispose() {
+        nameController.dispose();
+        super.dispose();
+    }
+
+    @override
     Widget build(BuildContext context){
-        return const Scaffold(
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Lesson 2'),
+            ), 
             body: Center(
-                child: Text("Hello Flutter!"),
+                child: Column(
+                    mainAxisSize: MainAxisSize.min, 
+                    children: [Text('Counter: $counter'), 
+                        const SizedBox(height: 12), 
+                        Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                                IconButton(
+                                    onPressed: decrement,
+                                    icon: const Icon(Icons.remove),
+                                ),
+                                IconButton(
+                                    onPressed: increment,
+                                    icon: const Icon(Icons.add),
+                                ),
+                            ],
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.all(12),
+                            child: TextField(
+                                controller: nameController,
+                                decoration: const InputDecoration(
+                                    labelText: 'Enter a name',
+                                    border: OutlineInputBorder(),
+                                )
+                            )
+                        ),
+                        //IconButton(
+                            //onPressed: addName,
+                           // icon: const Icon(Icons.add)
+                        //),
+                        FilledButton(
+                            onPressed: addName,
+                            Text: const Text('Add')
+                        ), 
+                        SizedBox(
+                            height: 200,
+                            child: ListView.builder(
+                                itemCount: names.length,
+                                itemBuilder: (context, index){
+                                    final String name = names[index];
+
+                                    return ListTile(
+                                        title: Text(name),
+                                        onTap: (){
+                                            setState(() {
+                                                names.removeAt(index);
+                                            });
+                                        },
+                                    ),
+                                },
+                            ),
+                        ),
+                        ],
+                        
+                ),
             ),
         );
     }
+
 }
+ 
